8d66b1e9f8487a3ef4e2b74f83ce85ac06798afd
Emanuel <78040463+Emanuel600@users.noreply.github.com>
2021-07-09 18:43:46 -0300

    Update fila.c

--- Tarefa 07/fila.c
+++ Tarefa 07/fila.c
@@ -1,14 +1,14 @@
-/** Parte do programa que contém as funções
- * de leitura e manipulação de pilhas
+/** Parte do programa que contÃ©m as funÃ§Ãµes
+ * de leitura e manipulaÃ§Ã£o de pilhas
  *
  * Define a estrutura das pilhas e como
- *cada função deve ler/manipular elas
+ *cada funÃ§Ã£o deve ler/manipular elas
  */
 
 // Includes do sistema
 #include <stdlib.h>
 #include <stdio.h>
-// Includes do módulo
+// Includes do mÃ³dulo
 #include "lista_enc.h"
 #include "fila.h"
 #include "no.h"
@@ -17,14 +17,14 @@
 #define VERDADEIRO 1
 
 struct filas {
-	lista_enc_t *dados; // Variáveis de controle estão dentro da lista
+	lista_enc_t *dados; // VariÃ¡veis de controle estÃ£o dentro da lista
 };
 
 
 //cria uma pilha generica
 fila_t * cria_fila (void){
 	fila_t* fila = NULL;
-	// Alocar memória
+	// Alocar memÃ³ria
 	fila = (fila_t*)malloc(sizeof(fila_t));
 
 	fila->dados = criar_lista_enc();
@@ -52,11 +52,14 @@ void *dequeue(fila_t *fila){
 
 // Libera elementos da fila
 void libera_fila(fila_t* fila){
+    while (!fila_vazia(fila) ){
+	    void* dado = dequeue(fila);
+	    free dado;
     free(fila->dados);
     free(fila);
 }
 
-// Retorna 1 se a fila é vazia
+// Retorna 1 se a fila Ã© vazia
 int fila_vazia(fila_t *fila){
 	if (fila == NULL){
 		perror("Fila inexistente");
